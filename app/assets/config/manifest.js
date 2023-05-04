e188665d48f39a4a4a558522f8a99feea76e7a73
Muhammad Adil (02264) <adil.muhammad@nxb.com.pk>
2023-05-04 12:25:28 +0500

    Removed extra items from js bundle

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,3 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link jquery.min.js
-//= link jquery_ujs.js
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,4 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "jquery", to: "jquery.min.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin_all_from "app/javascript/plugins", under: "plugins"
