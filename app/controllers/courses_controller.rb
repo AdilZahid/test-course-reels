c9332d85d3b5ea831b3798d71d7bc097ab66d19f
Mudassar Husnain <mudassar.husnain@nxb.com.pk>
2023-05-19 12:24:34 +0500

    fix course delete button issue

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,10 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_course, only: %i[ show edit update destroy ]
+
   def index
-    @courses = Course.all.reverse
+    @courses = Course.all
+    @courses = @courses.reverse
   end
 
   def new
@@ -22,7 +26,20 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
   end
 
+  def destroy
+    respond_to do |format|
+      if @course.destroy
+        format.html { redirect_to courses_url, notice: "Project was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    end
+  end
+
   private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :script)
